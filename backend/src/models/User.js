d92c9619c8daf99f09a62f26c0126bccb0efb06a
Sadid Alam <sadidalam14@gmail.com>
2025-07-17 22:06:57 +0530

    backend: login controller done

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -22,6 +22,7 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
+// hash password before saving
 UserSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
@@ -29,6 +30,11 @@ UserSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
+
+// compare password
+UserSchema.methods.comparePassword = async function (userPassword) {
+  return await bcrypt.compare(userPassword, this.password);
+};
 
 const User = mongoose.model("User", UserSchema);
 
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -69,8 +69,42 @@ router.post("/register", async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
-router.post("/login", (req, res) => {
-  res.send("login");
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    // check if user is exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // check if password is correct
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // generate token
+    const token = generateToken(user._id);
+
+    res.status(200).json({
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        profileImage: user.profileImage,
+      },
+    });
+  } catch (error) {
+    console.log("Error in login route", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 export default router;
