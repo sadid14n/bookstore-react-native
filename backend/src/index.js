c4ff1db0a4951e3386cfa68943a3ee059390fcb4
Sadid Alam <sadidalam14@gmail.com>
2025-07-18 00:20:41 +0530

    backend: get user books & delete books controller done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv/config";
+import cors from "cors";
 
 import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
@@ -9,6 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -61,4 +61,44 @@ router.get("/", protectRoute, async (req, res) => {
   }
 });
 
+// get recommended books by the logged in user
+router.get("/user", protectRoute, async (req, res) => {
+  try {
+    const books = await Book.find({ user: req.user._id }).sort({
+      createdAt: -1,
+    });
+
+    res.status(200).send(books);
+  } catch (error) {
+    console.log("Error fetching user books: ", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+router.delete("/:id", protectRoute, async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    // check if the user is the owner of the book
+    if (book.user.toString() !== req.user._id.toString()) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    // delete the image from cloudinary
+    if (book.image && book.image.includes("cloudinary")) {
+      try {
+        const publicId = book.image.split("/").pop().split(".")[0];
+        await cloudinary.uploader.destroy(publicId);
+      } catch (deleteError) {
+        console.log("Error deleting image from cloudinary: ", deleteError);
+      }
+    }
+    await book.remove();
+    res.status(200).json({ message: "Book deleted successfully" });
+  } catch (error) {}
+});
+
 export default router;
