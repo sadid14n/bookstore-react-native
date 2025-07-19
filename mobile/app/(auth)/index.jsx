6010197921b245777b190aae3d36a7404a996e29
Sadid Alam <sadidalam14@gmail.com>
2025-07-19 22:42:40 +0530

    frontend: setup auth screen, colors, styles

---
+++ mobile/app/(auth)/index.jsx
@@ -0,0 +1,53 @@
+import { View, Text, Image, TextInput } from "react-native";
+import styles from "../../assets/styles/login.styles";
+import { useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import COLORS from "../../constants/color";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleLogin = () => {};
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.topIllustration}>
+        <Image
+          source={require("../../assets/images/login.png")}
+          style={styles.illustrationImage}
+          resizeMode="contain"
+        />
+      </View>
+
+      <View style={styles.card}>
+        <View style={styles.formContainer}>
+          <View style={styles.inputGroup}>
+            <Text style={styles.label}>Email</Text>
+            <View style={styles.inputContainer}>
+              <Ionicons
+                name="mail-outline"
+                size={20}
+                color={COLORS.primary}
+                style={styles.inputIcon}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="Enter your email"
+                placeholderTextColor={COLORS.placeholderText}
+                value={email}
+                onChangeText={setEmail}
+                keyboardType="email-address"
+                autoCapitalize="none"
+              />
+            </View>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Login;
--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,9 +1,13 @@
+import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={{ fontSize: 68, fontWeight: "bold" }}>Hello</Text>
+
+      <Link href="/(auth)/signup">Signup</Link>
+      <Link href="/(auth)">Login</Link>
     </View>
   );
 }
