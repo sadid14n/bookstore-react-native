47be74da3f52251aad5294ddbc010d5366f1712f
Sadid Alam <sadidalam14@gmail.com>
2025-07-21 22:52:45 +0530

    backend: fix cron in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv/config";
 import cors from "cors";
-import job from "./lib/corn.js";
+import job from "./lib/cron.js";
 
 import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
