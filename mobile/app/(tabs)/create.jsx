0098cfc9b2ba48c5e79df1d319c2f74d39bdab5d
Sadid Alam <sadidalam14@gmail.com>
2025-07-23 11:47:02 +0530

    frontend: design create review page, added image upload functionality

--- mobile/app/(tabs)/create.jsx
+++ mobile/app/(tabs)/create.jsx
@@ -1,10 +1,210 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  Alert,
+  ActivityIndicator,
+} from "react-native";
+import React, { useState } from "react";
+import { useRouter } from "expo-router";
+import styles from "../../assets/styles/create.styles";
+import { Ionicons } from "@expo/vector-icons";
+import COLORS from "../../constants/color";
+
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
 
 export default function Create() {
+  const [title, setTitle] = useState("");
+  const [caption, setCaption] = useState("");
+  const [rating, setRating] = useState(3);
+  // to display the selected image
+  const [image, setImage] = useState(null);
+  const [imageBase64, setImageBase64] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+
+  const pickImage = async () => {
+    try {
+      // request permission if needed
+      if (Platform.OS !== "web") {
+        const { status } =
+          await ImagePicker.requestMediaLibraryPermissionsAsync();
+        console.log({ status });
+        if (status !== "granted") {
+          Alert.alert(
+            "Permission denied",
+            "We need camera roll permission to upload an image"
+          );
+          return;
+        }
+      }
+
+      // launch image library
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: "images",
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 0.5, // lower the image quality for smaller base64 string
+        base64: true,
+      });
+
+      if (!result.canceled) {
+        // console.log("result is here", result);
+        setImage(result.assets[0].uri);
+      }
+
+      // if base 64 provided use it
+      if (result.assets[0].base64) {
+        setImageBase64(result.assets[0].base64);
+      } else {
+        // otherwise, convert to base 64
+        const base64 = await FileSystem.readAsStringAsync(
+          result.assets[0].uri,
+          {
+            encoding: FileSystem.EncodingType.Base64,
+          }
+        );
+        setImageBase64(base64);
+      }
+    } catch (error) {
+      console.error("Error picking image", error);
+      Alert.alert("Error", "There was a problem selecting your image");
+    }
+  };
+
+  const handleSubmit = async () => {};
+
+  const renderRatingPicker = () => {
+    const stars = [];
+
+    for (let i = 1; i <= 5; i++) {
+      stars.push(
+        <TouchableOpacity
+          key={i}
+          onPress={() => setRating(i)}
+          style={styles.starButton}
+        >
+          <Ionicons
+            name={i <= rating ? "star" : "star-outline"}
+            size={32}
+            color={i <= rating ? "#f4b400" : COLORS.textSecondary}
+          />
+        </TouchableOpacity>
+      );
+    }
+    return <View style={styles.ratingContainer}>{stars}</View>;
+  };
+
   return (
-    <View>
-      <Text>create</Text>
-    </View>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView
+        contentContainerStyle={styles.container}
+        style={styles.scrollViewStyle}
+      >
+        <View style={styles.card}>
+          {/* HEADER */}
+          <View style={styles.header}>
+            <Text style={styles.title}>Add Book Recomandation</Text>
+            <Text style={styles.subtitle}>
+              Share your favourite reads with others
+            </Text>
+          </View>
+
+          {/* FORM */}
+          <View style={styles.form}>
+            {/* BOOK TITLE */}
+            <View style={styles.formGroup}>
+              <Text style={styles.label}>Book Title</Text>
+              <View style={styles.inputContainer}>
+                <Ionicons
+                  name="book-outline"
+                  size={20}
+                  color={COLORS.textSecondary}
+                  style={styles.inputIcon}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="Enter book title"
+                  placeholderTextColor={COLORS.placeholderTextColor}
+                  value={title}
+                  onChangeText={setTitle}
+                />
+              </View>
+            </View>
+
+            {/* RATING */}
+            <View style={styles.formGroup}>
+              <Text style={styles.label}>Your Rating</Text>
+              {renderRatingPicker()}
+            </View>
+
+            {/* IMAGE */}
+            <View style={styles.formGroup}>
+              <Text style={styles.label}>Book Image</Text>
+              <TouchableOpacity style={styles.imagePicker} onPress={pickImage}>
+                {image ? (
+                  <Image source={{ uri: image }} style={styles.previewImage} />
+                ) : (
+                  <View style={styles.placeholderContainer}>
+                    <Ionicons
+                      name="image-outline"
+                      size={40}
+                      color={COLORS.textSecondary}
+                    />
+                    <Text style={styles.placeholderText}>
+                      Tap to select image
+                    </Text>
+                  </View>
+                )}
+              </TouchableOpacity>
+            </View>
+
+            {/* CAPTION */}
+            <View style={styles.formGroup}>
+              <Text style={styles.label}>Caption</Text>
+              <TextInput
+                style={styles.textArea}
+                placeholder="Write your review or thoughts about this book..."
+                placeholderTextColor={COLORS.placeholderText}
+                value={caption}
+                onChangeText={setCaption}
+                multiline
+              />
+            </View>
+
+            {/* SUBMIT BUTTON */}
+            <TouchableOpacity
+              style={styles.button}
+              onPress={handleSubmit}
+              disabled={loading}
+            >
+              {loading ? (
+                <ActivityIndicator color={COLORS.white} />
+              ) : (
+                <>
+                  <Ionicons
+                    name="cloud-upload-outline"
+                    size={20}
+                    color={COLORS.white}
+                    style={styles.buttonIcon}
+                  />
+                  <Text style={styles.buttonText}>Share</Text>
+                </>
+              )}
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -3,7 +3,7 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import SafeScreen from "../components/SafeScreen";
 import { StatusBar } from "expo-status-bar";
 import { useAuthStore } from "./../store/authStore";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function RootLayout() {
   const router = useRouter();
@@ -11,12 +11,25 @@ export default function RootLayout() {
 
   const { checkAuth, user, token } = useAuthStore();
 
+  // useEffect(() => {
+  //   checkAuth();
+  // }, []);
+
+  const [isReady, setIsReady] = useState(false);
+
+  // Check auth only once when component mounts
   useEffect(() => {
-    checkAuth();
+    const initialize = async () => {
+      await checkAuth();
+      setIsReady(true); // Mark the layout as ready after auth check
+    };
+    initialize();
   }, []);
 
   // handle navigation based on the auth state
   useEffect(() => {
+    if (!isReady || segments.length === 0) return;
+
     const inAuthScreen = segments[0] === "(auth)";
     const isSignedIn = user && token;
 
