c45f179352269c18cf65e89da7299c005cb4b970
Sadid Alam <sadidalam14@gmail.com>
2025-07-26 11:23:20 +0530

    Increase app.use(express()) limit to 10mb

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 job.start();
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cors());
 
 app.use("/api/auth", authRoutes);
