efc7401fcfeae319723939c8d0ab6ce7e118ab99
Sadid Alam <sadidalam14@gmail.com>
2025-08-01 22:41:32 +0530

    frontend: added isAuthChecking State in useAuthStore

--- mobile/store/authStore.js
+++ mobile/store/authStore.js
@@ -6,6 +6,7 @@ export const useAuthStore = create((set) => ({
   user: null,
   token: null,
   isLoading: false,
+  isChekingAuth: true,
 
   register: async (username, email, password) => {
     set({ isLoading: true });
@@ -72,6 +73,8 @@ export const useAuthStore = create((set) => ({
       set({ token, user });
     } catch (error) {
       console.log("Auth check failed", error);
+    } finally {
+      set({ isChekingAuth: false });
     }
   },
 
