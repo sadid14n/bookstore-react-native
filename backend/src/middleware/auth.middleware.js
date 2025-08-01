21123b9a8c6587b014fceb9be0c167f2844de389
Sadid Alam <sadidalam14@gmail.com>
2025-08-01 22:39:34 +0530

    backend: modified auth middlewares

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -3,18 +3,24 @@ import User from "../models/User.js";
 
 const protectRoute = async (req, res, next) => {
   try {
-    // get token
-    const token = req.header("Authorization").replace("Bearer ", "");
-    if (!token)
+    const authHeader = req.header("Authorization");
+
+    console.log("Incoming Authorization Header:", authHeader); // DEBUG LINE
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
       return res
         .status(401)
         .json({ message: "No authentication token, access denied" });
+    }
 
-    // verify token
+    // const token = authHeader.replace("Bearer ", "").trim();
+    const token = authHeader.split(" ")[1];
+    console.log("🎯 Extracted token:", token);
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    // find user
     const user = await User.findById(decoded.userId).select("-password");
+
     if (!user)
       return res.status(401).json({ message: "No user found, access denied" });
 
